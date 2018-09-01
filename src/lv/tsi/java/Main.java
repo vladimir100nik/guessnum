78d10c155a6981e07a33efe550f05e048d61147f
vladimir100nik <vladimir100nik@gmail.com>
2018-09-01 21:09:59 +0300

    guessnum_01.09

--- src/lv/tsi/java/Main.java
+++ src/lv/tsi/java/Main.java
@@ -8,29 +8,39 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int myNum = rand.nextInt(100) + 1;
-        System.out.println(myNum);
+        System.out.println("Хочешь начать игру? Y/N");
+        String answer = scan.next();
+        while (answer.equals("y")) {
 
-        boolean userlost = true;
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Попытка N" + i);
-            int usernum = scan.nextInt();
 
-            if (usernum < myNum) {
-                System.out.println("Ваше число меньше!");
-            } else if (usernum > myNum) {
-                System.out.println("Ваше число больше!");
-            } else {
-                System.out.println("Вы угадали!!!");
-                userlost = false;
-                break;
+            int myNum = rand.nextInt(100) + 1;
+            System.out.println(myNum);
+
+
+            boolean userlost = true;
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Попытка N" + i);
+                int usernum = scan.nextInt();
+
+                if (usernum < myNum) {
+                    System.out.println("Ваше число меньше!");
+                } else if (usernum > myNum) {
+                    System.out.println("Ваше число больше!");
+                } else {
+                    System.out.println("Вы угадали!!!");
+                    userlost = false;
+                    break;
+                }
+
+            }
+            if (userlost == true) {
+                System.out.println("Вы проиграли");
             }
 
-        }
-        if(userlost == true) {
-            System.out.println("Вы проиграли");
-
+        System.out.println("Хочешь повторить? Y/N");
+        answer = scan.next();
+    }
         }
     }
 
-}
+
